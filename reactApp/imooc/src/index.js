4d622e428d91ec18e8ed56cf1ef1e432e31fc2c5
myHonor <myhonor@myHonordeMacBook-Pro.local>
2018-07-10 18:22:43 +0800

    modify code

--- reactApp/imooc/src/index.js
+++ reactApp/imooc/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDom from 'react-dom'
+import {createStore} from 'redux'
+import App from './App'
+import {counter} from './index.redux'
+import { addGUN,removeGUN } from './index.redux'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const store = createStore(counter)
+
+function render () {
+    ReactDom.render(<App store={store} addGUN={addGUN} removeGUN={removeGUN}/> ,document.getElementById('root'))
+}
+render()
+store.subscribe(render)
+
